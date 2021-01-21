304f1db011c236798a8eee7b31c6d05cbc0be422
Wyatt <wyattowalsh@gmail.com>
2021-01-20 23:43:32 -0800

    trying to add .js for ga

---
+++ data-science-notes/_static/gtag.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VKV4WP95W2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-VKV4WP95W2');
+</script>
